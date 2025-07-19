caa60136ed0cec1d8af0b0a41bcecd81f3f34d17
Hakuz0r <Hakuz0r@yandex.ru>
2025-07-20 00:30:50 +0300

    Add files via upload

---
+++ VeraVla/js/main.js
@@ -0,0 +1,83 @@
+// modal window
+
+(function () {
+    document.addEventListener('click', modal)
+    document.addEventListener('keydown', modal)
+    function modal(e) {
+        if (e.type === 'keydown') {
+            if (e.key === 'Escape') {
+                document.querySelectorAll('.modal').forEach(modal => {
+                    modal.classList.remove('active')
+                });
+                document.activeElement.blur();
+            }
+        }
+
+        const modal = e.target.closest('.modal')
+        if (modal && e.target === modal) {
+            modal.classList.remove('active');
+            return;
+        }
+
+        const consultationButton = e.target.closest('.button')
+        const closeButton = e.target.closest('.modal__window-close')
+        const openButtonFirst = e.target.closest('.section__button-first')
+        const openButtonSecond = e.target.closest('.headphone-button')
+
+        if (consultationButton) {
+            document.querySelectorAll('.modal').forEach(modal => {
+                modal.classList.remove('active')
+            });
+        }
+
+        if (closeButton) {
+            const modalId = closeButton.getAttribute('data-modal');
+            if (modalId) {
+                const modalTarget = document.getElementById(modalId);
+                if (modalTarget) {
+                    modalTarget.classList.remove('active');
+                } else {
+                    console.error(`Modal with ${modalId} not found`)
+                }
+                return
+            }
+        };
+
+
+        if (openButtonFirst) {
+            const modalId = openButtonFirst.getAttribute('data-modal');
+            if (modalId) {
+                document.querySelectorAll('.modal').forEach(modal => {
+                    modal.classList.remove('active')
+                });
+
+                const modalTarget = document.getElementById(modalId);
+
+                if (modalTarget) {
+                    modalTarget.classList.add('active');
+                } else {
+                    console.error(`Modal with ${modalId} not found`);
+                }
+                return;
+            }
+        }
+
+        if (openButtonSecond) {
+            const modalId = openButtonSecond.getAttribute('data-modal');
+            if (modalId) {
+                document.querySelectorAll('.modal').forEach(modal => {
+                    modal.classList.remove('active')
+                });
+
+                const modalTarget = document.getElementById(modalId);
+
+                if (modalTarget) {
+                    modalTarget.classList.add('active');
+                } else {
+                    console.error(`Modal with ${modalId} not found`);
+                }
+                return;
+            }
+        };
+    }
+})();
